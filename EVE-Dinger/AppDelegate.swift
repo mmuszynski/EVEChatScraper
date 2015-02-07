7b42517ee78386de01b126f0be3fe33e6e49732a
Mike Muszynski <mmuszynski@gmail.com>
2015-02-06 23:03:00 -0500

    Initial Commit

---
+++ EVE-Dinger/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  EVE-Dinger
+//
+//  Created by Mike Muszynski on 2/6/15.
+//  Copyright (c) 2015 Mike Muszynski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
